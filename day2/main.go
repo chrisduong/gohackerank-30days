2385874f2af3446246c2e7743b9619f2c0def3dd
Chris Duong <chrisduong83@gmail.com>
2017-11-30 10:58:22 +0800

    Print the sum of both integer

--- day2/main.go
+++ day2/main.go
@@ -3,16 +3,26 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
 func main() {
-	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
 
 	var i uint64 = 4
-	var d float64 = 4.0
-	var s string = "HackerRank "
-
+	// var d float64 = 4.0
+	// var s string = "HackerRank "
+	var lines []string
 	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	// Declare second integer, double, and String variables.
+	i2, _ := strconv.Atoi(lines[0])
+
+	// Print the sum of both integer variables on a new line.
+	fmt.Println(i + uint64(i2))
 }
