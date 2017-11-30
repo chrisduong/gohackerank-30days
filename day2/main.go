752851b42c4d264c64b80c1f85871dcaffc964c2
Chris Duong <chrisduong83@gmail.com>
2017-11-30 17:27:20 +0800

    finish day2

--- day2/main.go
+++ day2/main.go
@@ -11,18 +11,32 @@ import (
 func main() {
 
 	var i uint64 = 4
-	// var d float64 = 4.0
-	// var s string = "HackerRank "
-	var lines []string
+	var d float64 = 4.0
+	var s string = "HackerRank "
+
 	scanner := bufio.NewScanner(os.Stdin)
 
+	// *END* of constraint block //
+
+	// Read and save an integer, double, and String to your variables.
+	var lines []string
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
 
 	// Declare second integer, double, and String variables.
-	i2, _ := strconv.Atoi(lines[0])
+	i2, _ := strconv.ParseUint(lines[0], 10, 64)
+	d2, _ := strconv.ParseFloat(lines[1], 64)
+	s2 := lines[2]
 
 	// Print the sum of both integer variables on a new line.
-	fmt.Println(i + uint64(i2))
+	fmt.Println(i + i2)
+
+	// Print the sum of the double variables on a new line.
+	fmt.Printf("%.1f\n", d+d2)
+
+	// Concatenate and print the String variables on a new line
+	// The 's' variable above should be printed first.
+	fmt.Println(s + s2)
+
 }
