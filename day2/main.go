db6c36e8d663028aba4465e51b2874fcc7849f64
Chris Duong <chrisduong83@gmail.com>
2017-11-30 10:40:36 +0800

    start day2

---
+++ day2/main.go
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/30-data-types/problem
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
+
+	var i uint64 = 4
+	var d float64 = 4.0
+	var s string = "HackerRank "
+
+	scanner := bufio.NewScanner(os.Stdin)
+}
